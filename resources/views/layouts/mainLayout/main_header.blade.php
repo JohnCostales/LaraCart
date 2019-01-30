4d6f6e7d099f82fd891222d5ac3ddeb722f4745f
John Costales <j.g.costales96@gmail.com>
2019-01-30 23:22:06 +0000

    Cleaned up layout

--- resources/views/layouts/mainLayout/main_header.blade.php
+++ resources/views/layouts/mainLayout/main_header.blade.php
@@ -5,7 +5,7 @@
                 <div class="col-sm-6">
                     <div class="contactinfo">
                         <ul class="nav nav-pills">
-                            <li><a href="#"><i class="fa fa-phone"></i> +2 95 01 88 821</a></li>
+                            <li><a href="#"><i class="fa fa-phone"></i>+353 086 012 3456</a></li>
                             <li><a href="#"><i class="fa fa-envelope"></i> info@domain.com</a></li>
                         </ul>
                     </div>
